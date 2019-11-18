e259cc73c3f0b196a1fae5648d72fe1ed45a180f
Ken Reed <kenakioreed@gmail.com>
2019-11-17 19:58:48 -0500

    cleanup

--- src/game.rs
+++ src/game.rs
@@ -68,7 +68,7 @@ impl Game{
 		let bot_prop=BotProp{
             radius:Dist::new(12.0),
             collision_drag:0.003,
-            collision_push:0.5,
+            collision_push:0.1,
             minimum_dis_sqr:0.0001,
             viscousity_coeff:0.03
         };
@@ -76,7 +76,9 @@ impl Game{
         let num_bot=1000;
         let s=dists::grid::Grid::new(*dim.clone().grow(-0.1),num_bot);
     	let bots:Vec<GridBot>=s.take(num_bot).map(|pos|{
-    		let bot=Bot::new(vec2(pos.x as f32,pos.y as f32));
+    		let mut bot=Bot::new(vec2(pos.x as f32,pos.y as f32));
+    		//bot.vel.y=-1.0;
+    		//bot.vel.x=1.0;
     		GridBot{bot,state:GridBotState::DoingNothing}
     	}).collect();
 
@@ -98,7 +100,7 @@ impl Game{
 	}
 
 	pub fn step(&mut self){
-
+		
 		let mut path_requests=Vec::new();
 		for (i,b) in self.bots.iter_mut().enumerate(){
 			if b.state ==GridBotState::DoingNothing{
@@ -172,12 +174,15 @@ impl Game{
         dinotree_alg::colfind::QueryBuilder::new(&mut tree).query_par(|mut a,mut b|{
             bot_prop.collide(&mut a.inner_mut().bot,&mut b.inner_mut().bot);
         });
+		
+        
 
 
 		for b in self.bots.iter_mut(){
 			
-			let target_radius=self.grid.cell_radius().x;
 
+			let target_radius=self.grid.cell_radius().x;
+			
 			match &mut b.state{
 				GridBotState::Moving(ref mut pointiter)=>{
 
@@ -204,276 +209,154 @@ impl Game{
 					b.bot.move_to_point(b.bot.pos,target_radius);
 				}
 			}
-
-			//use crate::grid::raycast::RayCaster;
-			//use crate::grid::raycast::CollideCellEvent;
-
 			
-			
+
+
+			//Get square to display the 4 ray casts.
+			//Confirm you get different values of tval for each.
+			//
+
+
+
+
+			let mut skip_update=false;
 			let bot=&mut b.bot;
-			let ray=duckduckgeo::Ray{point:bot.pos,dir:bot.vel};
-			
+			match self.walls.get_option(self.grid.to_grid(bot.pos)){
+				Some(walls)=>{
+					if !walls{
+						if bot.vel.magnitude2()>0.0{
+							
+							
+							let rect= bot.create_bbox(&self.bot_prop);
+							let corners=[
+								vec2(rect.x.left,rect.y.left),
+								vec2(rect.x.left,rect.y.right),
+								vec2(rect.x.right,rect.y.left),
+								vec2(rect.x.right,rect.y.right)
+							];
 
-			if let Some(caster)=RayCaster::new(&self.grid,ray){
-				for a in caster{
-						match self.walls.get_option(a.cell){
-							Some(wall)=>{
-								if a.tval<ray.dir.magnitude(){
-					
-									if wall{
-										//bounce_with_wall(&self.grid,&self.bot_prop,bot,&a);
-										break;
-									}
+							let mut results=Vec::new();
+							for &corner in corners.iter(){
+							
+								let a=cast_ray(&self.grid,&self.walls,corner,bot.vel.normalize_to(1.0),bot.vel.magnitude());	
+								if let Some(a)=a{
+									//bounce_with_wall(&self.grid,&self.bot_prop,bot,&a);
+									results.push((corner,a));
 								}
-							},
-							None=>{
-								break;
 							}
-						
+
+							/*
+							if !results.is_empty(){
+								dbg!(&results);
+							}
+							*/
+
+							match results.iter().min_by(|a,b|a.1.tval.partial_cmp(&b.1.tval).unwrap()){
+								Some(&(corner,a))=>{
+									let corner_diff=corner-bot.pos;
+									skip_update=true;
+									bounce_with_wall(&self.grid,&self.bot_prop,bot,corner_diff,&a);
+								},
+								None=>{
+
+								}
+							}
+							
+							
+							
+							
+						}
 					}
+				},
+				None=>{
+
 				}
 			}
 			
-			fn bounce_with_wall(grid_dim:&GridViewPort,bot_prop:&BotProp,bot:&mut Bot,collide:&CollideCellEvent){
-				use CardDir::*;
-
-				let current_cell=collide.cell;
-				let bottom_right=collide.cell+vec2(1,1);
-
-				let e=bot_prop.radius.dis()+0.001;
-	    		let current_cell_pos=grid_dim.to_world_topleft(current_cell);
-	    		let bottom_right_pos=grid_dim.to_world_topleft(bottom_right);
-
-	    		let slow=0.5;
-	    		
-				match collide.dir_hit{
-					L=>{
-						//hitting left
- 						bot.pos.x=current_cell_pos.x-e;
- 						bot.vel.x=-bot.vel.x;
- 						bot.vel.x*=slow;
-					},
-					R=>{
- 						//hitting right
- 						bot.pos.x=bottom_right_pos.x+e;
- 						bot.vel.x=-bot.vel.x;
- 						bot.vel.x*=slow;
-					},
-					U=>{
- 						//hitting top
- 						bot.pos.y=current_cell_pos.y-e;
- 						bot.vel.y=-bot.vel.y;
- 						bot.vel.y*=slow;
-					},
-					D=>{
-						//hitting bottom
- 						bot.pos.y=bottom_right_pos.y+e;
- 						bot.vel.y=-bot.vel.y;
- 						bot.vel.y*=slow;
-					}
-				}
+			//b.bot.pos+=b.bot.vel;
+			//b.bot.vel+=b.bot.acc;
+			//b.bot.acc=vec2same(0.);
+			if b.bot.vel.magnitude()>1.0{
+				b.bot.vel=b.bot.vel.normalize_to(1.0);
 			}
-			/*
-			fn collide_grid(bot_prop:&BotProp,bot:&mut Bot,grid_dim:&GridDim2D,walls:&Grid2D){
-			    
-			    use line_drawing::Bresenham;
-			    let start=grid_dim.convert_to_grid(bot.pos);
-			    let end=grid_dim.convert_to_grid(bot.pos+bot.vel);
-
-			    
-			    if walls.get(start){
-			    	//If we are in a wall already, just quit.
-			    	return;
-			    }
-
-
-			    let mut last=start;
-			    for (x, y) in Bresenham::new((start.x, start.y), (end.x, end.y)) {
-			    	let curr=vec2(x,y);
-			    	if walls.get(curr){
-			    		//We are hitting this bot, from this angle:
-			    		let angle=curr-last;
-			    		let e=bot_prop.radius.dis()+0.001;
-			    		let curr_cell_pos=grid_dim.convert_to_world_topleft(curr);
-			    		let last_cell_pos=grid_dim.convert_to_world_topleft(last);
-			    		let slow=0.5;
-			    		match angle{
-			    			Vec2{x:1,y:1} |
-			    			Vec2{x:1,y:0}=>{
-				    			//hitting left
-		 						bot.pos.x=curr_cell_pos.x-e;
-		 						bot.vel.x=-bot.vel.x;
-		 						bot.vel.x*=slow;
-			    			},
-			    			Vec2{x:-1,y:-1}|
-			    			Vec2{x:-1,y:0}=>{
-		 						//hitting right
-		 						bot.pos.x=last_cell_pos.x+e;
-		 						bot.vel.x=-bot.vel.x;
-		 						bot.vel.x*=slow;
-			    			},
-			    			Vec2{x:-1,y:1}|
-			    			Vec2{x:0,y:1}=>{
-		 						//hitting top
-		 						bot.pos.y=curr_cell_pos.y+e;
-		 						bot.vel.y=-bot.vel.y;
-		 						bot.vel.y*=slow;
-			    			},
-			    			Vec2{x:1,y:-1}|
-			    			Vec2{x:0,y:-1}=>{
-								//hitting bottom
-		 						bot.pos.y=last_cell_pos.y-e;
-		 						bot.vel.y=-bot.vel.y;
-		 						bot.vel.y*=slow;
-			    			},
-			    			_=>{
-			    				unreachable!()
-			    			}
-
-			    		}
-			    		//We hit a wall, return.
-			    		return;
-			    	}
-			    	last=curr;
-			    }
-			}
-
-
-			collide_grid(&self.bot_prop,&mut b.bot,&self.grid,&self.walls);
-			*/
-
-			//let bot=&mut b.bot;
-			//let ray=duckduckgeo::Ray{point:bot.pos,dir:bot.vel};
-			//ray_compute_intersection_tvalue(&self.grid,ray);
-			/*
-			//assumes that bot is smaller than a grid cell!!!!!!
-			fn handle(prop:&BotProp,bot:&mut Bot,grid:&GridDim2D){
-				let cell_curr = grid.convert_to_grid(bot.pos);
- 				//let cell_next = grid.convert_to_grid(bot.pos+bot.vel);
- 				let cell_curr_pos = grid.convert_to_world_topleft(cell_curr);
- 				//let cell_next_pos = grid.convert_to_world_topleft(cell_next);
-
- 				let top=bot.create_bbox(prop).y.left;
-				let bottom=bot.create_bbox(prop).y.right;
-				let left=bot.create_bbox(prop).x.left;
-				let right=bot.create_bbox(prop).x.right;
-				let radius=prop.radius.dis();
-				let e=radius+0.001;
-				
-				{//handle y axis
-					let vely=bot.vel.y;
-					let pp=if vely>0.0{
-						bot.pos.y+vely+radius
-					}else{
-						bot.pos.y+vely-radius
-					};
-					
-					let nexta=grid.inner.get(grid.convert_to_grid(vec2(left,pp)));
-					let nextb=grid.inner.get(grid.convert_to_grid(vec2(right,pp)));
-					
-					let nn=grid.convert_to_world_topleft(grid.convert_to_grid(vec2(left,pp)));
-										
-
-					if nexta|nextb{
-						if vely>=0.0{	
-							bot.pos.y=nn.y-e;
-						}else{					
-							bot.pos.y=cell_curr_pos.y+e;
-						}
-						bot.vel.y=-bot.vel.y;
-						bot.vel.y*=0.5;
-					}
-				}
-
-				{//handle x axis
-					let velx=bot.vel.x;
-					let pp=if velx>0.0{
-						bot.pos.x+velx+radius
-					}else{
-						bot.pos.x+velx-radius
-					};
-					
-					let nexta=grid.inner.get(grid.convert_to_grid(vec2(pp,top)));
-					let nextb=grid.inner.get(grid.convert_to_grid(vec2(pp,bottom)));
-					
-					let nn=grid.convert_to_world_topleft(grid.convert_to_grid(vec2(pp,top)));
-										
-
-					if nexta|nextb{
-						if velx>=0.0{	
-							bot.pos.x=nn.x-e;
-						}else{					
-							bot.pos.x=cell_curr_pos.x+e;
-						}
-						bot.vel.x=-bot.vel.x;
-						bot.vel.x*=0.5;
-					}
-				}
-			}
-			*/
-
-			//handle(&self.bot_prop,&mut b.bot,&self.grid);
-			
-			/*
-			let curr_in_wall=self.grid.inner.get(self.grid.convert_to_grid(b.bot.pos));
-			let next_in_wall=self.grid.inner.get(self.grid.convert_to_grid(b.bot.pos+b.bot.vel));
-			if !curr_in_wall && next_in_wall{
-				//we are about to move into a wall.
-				let cell_curr = self.grid.convert_to_grid(b.bot.pos);
- 				let cell_next = self.grid.convert_to_grid(b.bot.pos+b.bot.vel);
-
- 				let cell_curr_pos = self.grid.convert_to_world_topleft(cell_curr);
- 				let cell_next_pos = self.grid.convert_to_world_topleft(cell_next);
-
-
- 				let diff=(cell_next-cell_curr);
-
- 				
- 				let arr = if diff.abs().x+diff.abs().y==2{
- 					let k=diff.split_into_components();
- 					vec!(k[0],k[1])
- 				}else{
- 					vec!(diff)
- 				};
-
- 				let e=0.001;
- 				for a in arr.iter(){
-	 				match a{
-	 					Vec2{x:-1,y:0}=>{
-	 						//hitting left
-	 						b.bot.pos.x=cell_curr_pos.x+e;
-	 						b.bot.vel.x=-b.bot.vel.x;
-	 					},
-	 					Vec2{x:1,y:0}=>{
-	 						//hitting right
-	 						b.bot.pos.x=cell_next_pos.x-e;
-	 						b.bot.vel.x=-b.bot.vel.x;
-	 					},
-	 					Vec2{x:0,y:-1}=>{
-	 						//hitting top
-	 						b.bot.pos.y=cell_curr_pos.y+e;
-	 						b.bot.vel.y=-b.bot.vel.y;
-	 					},
-	 					Vec2{x:0,y:1}=>{
-	 						//hitting bottom
-	 						b.bot.pos.y=cell_next_pos.y-e;
-	 						b.bot.vel.y=-b.bot.vel.y;
-	 					},
-	 					_=>{
-	 						unreachable!("{:?}",a);
-	 					}
-	 				}
- 				}
-
- 				let curr_in_wall=self.grid.inner.get(self.grid.convert_to_grid(b.bot.pos));
-				let next_in_wall=self.grid.inner.get(self.grid.convert_to_grid(b.bot.pos+b.bot.vel));
-				assert!(!curr_in_wall && !next_in_wall);
-			}
-			*/
-
 			b.bot.update();
+			
+
+			
+
+		}
+	}
+}
 
 
+
+
+
+fn cast_ray(grid:&GridViewPort,walls:&Grid2D,point:Vec2<WorldNum>,dir:Vec2<WorldNum>,max_tval:WorldNum)->Option<CollideCellEvent>{
+
+	let ray=duckduckgeo::Ray{point,dir};
+	
+	match RayCaster::new(grid,ray){
+		Some(caster)=>{
+			for a in caster{
+				if a.tval<=max_tval{				
+					match walls.get_option(a.cell){
+						Some(wall)=>{
+							if wall{
+								return Some(a);
+							}		
+						},
+						None=>{
+							return None; //We've ray casted off the wall grid.
+						}		
+					}
+				}else{
+					return None;
+				}
+			}
+		},
+		None=>{
+			panic!("failed to make ray caster {:?}",ray);
+		}
+	}
+	None
+}
+
+fn bounce_with_wall(grid_dim:&GridViewPort,bot_prop:&BotProp,bot:&mut Bot,corner_diff:Vec2<WorldNum>,collide:&CollideCellEvent){
+
+
+	use CardDir::*;
+
+	let current_cell=collide.cell;
+	let bottom_right=collide.cell+vec2(1,1);
+
+	let e=bot_prop.radius.dis()+0.001;
+	let current_cell_pos=grid_dim.to_world_topleft(current_cell);
+	let bottom_right_pos=grid_dim.to_world_topleft(bottom_right);
+
+	let slow=0.2;
+	
+	match collide.dir_hit{
+		L=>{
+				bot.pos.x=current_cell_pos.x-corner_diff.x;
+				bot.vel.x=-bot.vel.x;
+				bot.vel.x*=slow;
+		},
+		R=>{
+				
+				bot.pos.x=bottom_right_pos.x-corner_diff.x;
+				bot.vel.x=-bot.vel.x;
+				bot.vel.x*=slow;
+		},
+		U=>{
+				bot.pos.y=current_cell_pos.y-corner_diff.y;
+				bot.vel.y=-bot.vel.y;
+				bot.vel.y*=slow;
+		},
+		D=>{
+				bot.pos.y=bottom_right_pos.y-corner_diff.y;
+				bot.vel.y=-bot.vel.y;
+				bot.vel.y*=slow;
 		}
 	}
 }
